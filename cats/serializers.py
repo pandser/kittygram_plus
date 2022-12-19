aaafc6957c67c21a11be55bd64cd015f59a16c22
Андрей Полушин <pandser@yandex.ru>
2022-12-19 21:19:45 +0300

    kittygramm with JWT

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,75 @@
+import datetime as dt
+
+#import webcolors
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
+
+
+# class Hex2NameColor(serializers.Field):
+#     def to_representation(self, value):
+#         return value
+#
+#     def to_internal_value(self, data):
+#         try:
+#             data = webcolors.hex_to_name(data)
+#         except ValueError:
+#             raise serializers.ValidationError('Для этого цвета нет имени.')
+#         return data
+
+
+class AchievementsSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    achievements = AchievementsSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        if 'achivements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement
+            )
+            AchievementCat.objects.create(
+                achievement=current_achievement,
+                cat=cat,
+            )
+        return cat
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,18 @@
 from django.urls import include, path
 
+import djoser
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
